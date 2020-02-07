b00602991141ee20238dc5a5a775d8be54a81b45
Håkan Nylén <hakan@dun.se>
2020-02-07 11:37:14 +0100

    fixed http status fixes

--- src/handlers/git_handler.cr
+++ src/handlers/git_handler.cr
@@ -2,21 +2,20 @@ class HTTP::GitHandler
   include HTTP::Handler
 
   def call(context)
-    logger = Logger.new(STDOUT)
-    logger.info "path: #{context.request.path}"
     if /[-\/\w\.]+\.git/.match(context.request.path)
-
-      logger.info "headers: #{context.request.headers}"
       if context.request.headers.has_key?("Authorization")
         _, enc = context.request.headers["Authorization"].split
         username, password = Base64.decode_string(enc).split(":")
         if authorized?(username, password)
           context.request.headers.delete("Authorization")
           git = GitServer.new(context)
-          git.call
+          return git.call
         end
       end
-      context.response.respond_with_status(HTTP::Status.new(403), "Forbidden")
+      context.response.headers.add("WWW-Authenticate", "Basic realm=\"User Visible Realm\"")
+      context.response.status = HTTP::Status.new(401)
+      context.response.puts "Forbidden"
+      context.response.close
     else
       call_next(context)
     end
