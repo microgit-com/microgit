abb4cc9481185013cf225f1625d68111c964bf63
Håkan Nylén <hakan@dun.se>
2020-03-18 20:18:04 +0100

    added repos to user profile

--- src/actions/namespaces/show.cr
+++ src/actions/namespaces/show.cr
@@ -5,7 +5,7 @@ class Namespaces::Show < BrowserAction
     namespace = get_namespace
     item = namespace.item
     if item.is_a?(User)
-      html ShowUserPage, user: item
+      html ShowUserPage, user: item, repositories: RepositoryQuery.new.preload_user.preload_team.user_id(item.id)
     elsif item.is_a?(Team)
       html ShowTeamPage, team: item, repositories: RepositoryQuery.new.preload_user.preload_team.team_id(item.id)
     else
--- src/pages/namespaces/show_user_page.cr
+++ src/pages/namespaces/show_user_page.cr
@@ -1,8 +1,10 @@
 class Namespaces::ShowUserPage < MainLayout
   needs user : User
+  needs repositories : RepositoryQuery
   quick_def page_title, @user.username
+  quick_def page_title_template, render_template("namespaces/show_user_info.html.ecr")
 
   def content
-    render_template "namespaces/show_user.html.ecr"
+    render_template "namespaces/user_repositories.html.ecr"
   end
 end
