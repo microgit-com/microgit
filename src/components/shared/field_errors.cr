1d7b0d46d12296c28408d44fbe5048465ea5d4d7
Håkan Nylén <hakan@dun.se>
2020-03-22 06:56:49 +0100

    some design stuff

--- src/components/shared/field_errors.cr
+++ src/components/shared/field_errors.cr
@@ -4,7 +4,7 @@ class Shared::FieldErrors(T) < BaseComponent
   # Customize the markup and styles to match your application
   def render
     unless @field.valid?
-      div class: "error" do
+      div class: "text-red-800 bg-red-200 p-2 rounded-sm mb-2" do
         label_text = Wordsmith::Inflector.humanize(@field.name.to_s)
         text "#{label_text} #{@field.errors.first}"
       end
